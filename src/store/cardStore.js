1bc468f893c8fcd30bbf279cd259a0b242028a54
Ryan <ryanpricelondon@gmail.com>
2021-04-12 12:41:51 -0400

    Added new store type

--- src/store/cardStore.js
+++ src/store/cardStore.js
@@ -3,6 +3,8 @@ import { dynamicSort, getRarity, soldOutSort } from "../helpers";
 
 const typeIdsOnChain = [];
 
+//push Apr 12,2021
+typeIdsOnChain.push(195);
 //push Apr 8,2021
 typeIdsOnChain.push(166,165,164);
 //push Apr 7,2021
